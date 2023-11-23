caeabdb047febca5833304506ff68dace2b1e2e3
uf206626 <panyuan.zhang@partner.lingyue-digital.com>
2023-11-23 23:13:27 +0800

    静态资源提交

--- src/icons/index.js
+++ src/icons/index.js
@@ -2,17 +2,12 @@ import Vue from 'vue'
 import SvgIcon from '@/components/SvgIcon'// svg component
 
 // register globally
-Vue.component('svg-icon', SvgIcon)  // 全局注册组件
+Vue.component('svg-icon', SvgIcon)
 
-// require.context(目标目录,是否扫描子目录,正则表达式) 扫描目录中得到文件
+// 下面三行代码的任务是 把 同级目录的 svg目录下的.svg图片引入到项目中来
 const req = require.context('./svg', false, /\.svg$/)
-console.log(req.keys())
-
 const requireAll = requireContext => requireContext.keys().map(requireContext)
-// 数组方法 map foreach
-// map 是循环每一项 svg图片 map(()=>{})
-// req 函数能够引用图片到项目中
-// 将所有的svg都引用到项目中
+requireAll(req)
 
-requireAll(req) // 调用函数
-
+// 相当于把svg下的所有的svg图片打包到了项目中
+// 如果想用svg图片 就在svg目录下去寻找就可以了
---
+++ test.js
@@ -0,0 +1,10 @@
+const test1 = ()=>{
+    var a = 1;
+    console.log(a);
+}
+
+const test2 = ()=>{
+    console.log(a);
+}
+
+test2()
